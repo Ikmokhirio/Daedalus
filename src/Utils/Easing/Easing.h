db0a7e00bcf6d5bb02b6a511d4a0938a902b70f1
Ikmokhirio <Ikmokhirio@mail.ru>
2022-07-16 21:13:37 +0300

    Move : Easing as files rather than submodule Easing library author : https://github.com/nicolausYes/easing-functions

---
+++ src/Utils/Easing/Easing.h
@@ -0,0 +1,42 @@
+#pragma once
+
+// Author https://github.com/nicolausYes/easing-functions
+
+enum easing_functions
+{
+    EaseInSine,
+    EaseOutSine,
+    EaseInOutSine,
+    EaseInQuad,
+    EaseOutQuad,
+    EaseInOutQuad,
+    EaseInCubic,
+    EaseOutCubic,
+    EaseInOutCubic,
+    EaseInQuart,
+    EaseOutQuart,
+    EaseInOutQuart,
+    EaseInQuint,
+    EaseOutQuint,
+    EaseInOutQuint,
+    EaseInExpo,
+    EaseOutExpo,
+    EaseInOutExpo,
+    EaseInCirc,
+    EaseOutCirc,
+    EaseInOutCirc,
+    EaseInBack,
+    EaseOutBack,
+    EaseInOutBack,
+    EaseInElastic,
+    EaseOutElastic,
+    EaseInOutElastic,
+    EaseInBounce,
+    EaseOutBounce,
+    EaseInOutBounce
+};
+
+typedef double(*easingFunction)(double);
+
+easingFunction getEasingFunction( easing_functions function );
+
